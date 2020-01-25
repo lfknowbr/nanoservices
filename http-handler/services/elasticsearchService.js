29d33b59335a663a3e73b3cc80818fef9dc65a91
Luis fernando <fernandocpdoba@gmail.com>
2020-01-25 00:37:52 -0300

    Alteração no endpoint do elasticsearch

--- http-handler/services/elasticsearchService.js
+++ http-handler/services/elasticsearchService.js
@@ -2,7 +2,7 @@ const elasticsearch = require('elasticsearch')
 
 const client = new elasticsearch.Client({
   apiVersion: '7.1',
-  host: 'https://vpc-elasticsearch-cluster-nanose-rcmpxeuflr5ysu43nrshxy2pre.us-east-1.es.amazonaws.com'
+  host: 'https://vpc-alasticsearch-curso-bux3jw2fmfuyxok77wzds74yem.us-east-1.es.amazonaws.com'
 })
 
 const search = async query => {
